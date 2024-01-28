c80a967ce6901f4049d3089262b56e469bc40d82
Teguru <153816819+Teguru@users.noreply.github.com>
2024-01-28 15:41:28 +0000

    implemented a for few scientifc functions

--- src/srcipt.js
+++ src/srcipt.js
@@ -1,28 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Document is ready!');
-    const display = document.getElementById('calc-display');
-    const buttons = document.getElementsByClassName('btn');
+  console.log('Document is ready!');
+  const display = document.getElementById('calc-display');
+  const buttons = document.getElementsByClassName('btn');
 
-    let currentValue = "";
+  let currentValue = "";
 
-    function evaluateResult() {
-      console.log('currentValue', currentValue)
-      const convertedValue = currentValue
+  function evaluateResult() {
+    console.log('currentValue', currentValue)
+    const convertedValue = currentValue
       .replace("x", "*")
       .replace("÷", "/")
       .replace('%', '*0.01')
+      .replace('sin', 'Math.sin')
+      .replace('cos', 'Math.cos')
+      .replace('ln', 'Math.log')
+      .replace('π', 'Math.PI')
+      .replace('log', 'Math.log10')
+      .replace('e', 'Math.E')
+      .replace('tan', 'Math.tan')
+      .replace('√', 'Math.sqrt')
 
-      console.log('currentValue', convertedValue)
-      const result = eval(convertedValue);
-      currentValue = result.toString();
-      display.value = currentValue;
-    }
+    console.log('currentValue', convertedValue)
+    const result = eval(convertedValue);
+    currentValue = result.toString();
+    display.value = currentValue;
+  }
 
-    for (let i = 0; i < buttons.length; i++) {
-      const button = buttons[i];
-      button.addEventListener('click', function () {
-        const value = button.innerText;
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', function () {
+      const value = button.innerText;
 
+      try {
         if (value == "AC") {
           currentValue = "";
           display.value = currentValue;
@@ -33,14 +42,21 @@ document.addEventListener('DOMContentLoaded', function () {
           console.log('currentValue:', currentValue);
           display.value = currentValue;
         }
+      } catch (error) {
+        console.error(error);
+        currentValue = "ERROR"
+        display.value = currentValue
+      }
+
+        
 
 
       })
 
-    }
+  }
 
 
 
 
 
-  });
+});
